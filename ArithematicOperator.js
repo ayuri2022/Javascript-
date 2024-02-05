ac7feb3ff8623708d0b719af1aca92c2562287fa
Mayuri Goltakke <109478633+ayuri2022@users.noreply.github.com>
2024-02-05 23:59:44 +0530

    Add files via upload

---
+++ ArithematicOperator.js
@@ -0,0 +1,33 @@
+// operator in javascript
+
+let a = 12;
+let b = 2;
+
+//Addition
+
+console.log("a+b =" , a+b);
+
+//substrction 
+console.log("a-n =",a-b);
+
+//division
+
+console.log("a/b =",a/b);
+
+//multiplication
+console.log("a*b =",a*b);
+
+//exponentiation
+
+console.log("a**b =",a**b);
+
+//modulus
+console.log("a%b =",a%b);
+
+//increment
+console.log("++a =",++a) ; //pre increment
+console.log("a++ =" ,a++) ;//post increment
+
+//decrement
+console.log("--b=",--b);  //pre decrement
+console.log("b-- =",b--);
---
+++ comparisonOperator.js
@@ -0,0 +1,23 @@
+//comparison operator shows true and false result
+// equal to operator (==) . 
+
+let a =3;
+let b =4;
+console.log(a==b);   
+
+//not eaual to (!=)
+
+let x = 10;
+let y = 20;
+console.log(x!=y);
+
+//equal value type(===)
+
+let p = 34;
+let q ="x";
+console.log(p===q);
+
+//!==
+let num1 = 3;
+let num2 = 4;
+console.log(num1!==num2);
